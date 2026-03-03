d282ffc1bace974834f2fbd2816bbc08919774d4
valdez <leonardovaldez.sn@gmail.com>
2026-03-03 02:30:19 -0300

    Add CurrencyCodes class to set up constants for the codes

---
+++ src/CurrencyCodes.java
@@ -0,0 +1,6 @@
+public class CurrencyCodes {
+    public static final String real = "BRL";// Brazilian Real
+    public static final String yuan = "CNY"; // Chinese Yuan
+    public static final String won = "KRW"; // South Korean Won
+    public static final String peso =  "CUP"; // Cuban Peso
+}
